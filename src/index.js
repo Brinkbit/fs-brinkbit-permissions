ac1920abf725797026a2820818cf90a3d206e3be
Brian Silvia <brian@brinkbit.com>
2016-01-20 20:12:02 -0500

    justin's changes

--- src/index.js
+++ src/index.js
@@ -11,15 +11,15 @@ mongoose.Promise = global.Promise;
 
 function verifyPermissions( user, operation, file, isParent ) {
     // sanity check - verify the meta exists
-    Meta.find({ _id: file.metaDataId }).exec()
+    return Meta.find({ _id: file.metaDataId }).exec()
         .then(( meta ) => {
             // if the meta does not exist, something has gone very wrong
             if ( !meta ) {
-                Promise.reject( 'INVALID_RESOURCE' );
+                return Promise.reject( 'INVALID_RESOURCE' );
             }
             // if this is a parent, we need to make sure it's a folder
             else if ( isParent && meta.mimeType !== 'folder' ) {
-                Promise.reject( 'PARENT_IS_NOT_A_FOLDER' );
+                return Promise.reject( 'PARENT_IS_NOT_A_FOLDER' );
             }
             // if it passes all the above, return the permissions
             else {
@@ -29,25 +29,25 @@ function verifyPermissions( user, operation, file, isParent ) {
         })
         .then(( permissions ) => {
             if ( !permissions ) {
-                Promise.reject( 'USER_HAS_NO_PERMISSIONS_ON_THIS_OBJECT' );
+                return Promise.reject( 'USER_HAS_NO_PERMISSIONS_ON_THIS_OBJECT' );
             }
             else {
                 // time to run the permissions
                 if ( operation === 'read' &&
                     !permissions.read ) {
-                    Promise.reject( 'USER_DOES_NOT_HAVE_READ_PERMISSIONS_ON_THIS_OBJECT' );
+                    return Promise.reject( 'USER_DOES_NOT_HAVE_READ_PERMISSIONS_ON_THIS_OBJECT' );
                 }
                 else if ( operation === 'write' || 'update' &&
                     !permissions.write ) {
-                    Promise.reject( 'USER_DOES_NOT_HAVE_WRITE_PERMISSIONS_ON_THIS_OBJECT' );
+                    return Promise.reject( 'USER_DOES_NOT_HAVE_WRITE_PERMISSIONS_ON_THIS_OBJECT' );
                 }
                 else if ( operation === 'destroy' &&
                     !permissions.destroy ) {
-                    Promise.reject( 'USER_DOES_NOT_HAVE_DESTROY_PERMISSIONS_ON_THIS_OBJECT' );
+                    return Promise.reject( 'USER_DOES_NOT_HAVE_DESTROY_PERMISSIONS_ON_THIS_OBJECT' );
                 }
                 else {
                     // if we've made it this far, we're good to go!
-                    Promise.resolve();
+                    return Promise.resolve();
                 }
             }
         });
@@ -57,20 +57,19 @@ module.exports.verify = ( user, operation, fullPath ) => {
     // determine if this file (full path) currently exists
     File.findOne({ $and: [{ name: fullPath }, { userId: user }] }).exec()
     .then(( file ) => {
-        // a file must exist for certian operations
+        // a file must exist for certain operations
         if ( !file && operation === 'read' || 'update' || 'destroy' ) {
-            Promise.reject( 'object does not exist' );
+            return Promise.reject( 'object does not exist' );
         }
 
         // can't exist for write
         else if ( file && operation === 'write' ) {
-            Promise.reject( 'object already exists at that path' );
-            // perform verification on the parnet
+            return Promise.reject( 'object already exists at that path' );
         }
 
         // if this is a file, perform verification on the file
         if ( file ) {
-            verifyPermissions( user, operation, file, false );
+            return verifyPermissions( user, operation, file, false );
         }
         // otherwise perform verification on the parent
         else {
@@ -78,10 +77,10 @@ module.exports.verify = ( user, operation, fullPath ) => {
             .then(( file ) => {
                 // if the parent does not exist, we have a problem
                 if ( !file ) {
-                    Promise.reject( 'INVALID_RESOURCE_PATH' );
+                    return Promise.reject( 'INVALID_RESOURCE_PATH' );
                 }
                 else {
-                    verifyPermissions( user, operation, file, true );
+                    return verifyPermissions( user, operation, file, true );
                 }
             });
         }
