e7b526b8b577f9c93ad87895992be57bf4ab158e
JoshF <josh.f@ttbventures.com>
2016-01-20 19:47:46 -0500

    simplified to the max

--- src/index.js
+++ src/index.js
@@ -9,139 +9,81 @@ mongoose.Promise = global.Promise;
 
 /* eslint no-shadow: 0 */
 
-module.exports.verify = ( user, operation, fullPath ) => {
-    function verifyPath( path, userId ) {
-        let fileObj;
-        File.findOne({ $and: [{ name: path }, { userId }] }).exec()
-            .then(( obj ) => {
-                if ( !obj ) {
-                    throw new Error({
-                        reason: 'INVALID_RESOURCE_PATH',
-                    });
-                }
-                else {
-                    fileObj = obj;
-                    return obj.metaDataId;
-                }
-            })
-            .then(( id ) => {
-                // return the meta
-                return Meta.findOne({ _id: id }).exec();
-            })
-            // figure out if this is the last parent and throw new Error
-            .then(( meta ) => {
-                if ( meta ) {
-                    // figure out if this is the last parent and throw new Error
-                    if ( !meta.children ) {
-                        throw new Error({
-                            reason: 'LAST PARENT',
-                            lastParentPath: fileObj.name,
-                            meta,
-                        });
-                    }
-                }
-                else {
-                    throw new Error({
-                        reason: 'INVALID_RESOURCE_PATH',
-                    });
-                }
-            });
-    }
-
-    function verifyLastParent( meta, lastParentPath, fullPath, user ) {
-        let fileExists;
-        fileExists = ( lastParentPath === fullPath.split( '/' ).length );
-        Meta.find({ $and: [{ guid: meta.guid }, { mimeType: 'folder' }] }).exec()
-        .then(( folderFind ) => {
-            // if this is not an exact match, and the last path was not a folder, we have a problem
-            if ( !folderFind && fileExists ) {
-                throw new Error( 'tried to add object to file' );
+function verifyPermissions( user, operation, file, isParent ) {
+    // sanity check - verify the meta exists
+    Meta.find({ _id: file.metaDataId }).exec()
+        .then(( meta ) => {
+            // if the meta does not exist, something has gone very wrong
+            if ( !meta ) {
+                Promise.reject( 'INVALID_RESOURCE' );
             }
+            // if this is a parent, we need to make sure it's a folder
+            else if ( isParent && meta.mimeType !== 'folder' ) {
+                Promise.reject( 'PARENT_IS_NOT_A_FOLDER' );
+            }
+            // if it passes all the above, return the permissions
             else {
-                // get permissions
+                // get the permissions
                 return Permissions.find({ $and: [{ resourceId: meta._id }, { userId: user }] }).exec();
             }
         })
         .then(( permissions ) => {
-            if ( permissions ) {
-                throw new Error( 'user has no permissions on this object' );
+            if ( !permissions ) {
+                Promise.reject( 'USER_HAS_NO_PERMISSIONS_ON_THIS_OBJECT' );
             }
             else {
-                // we now know where our path ends and what our user's permissions are on that end. time to test things
-                // important vars:
-                // fileExists
-                // permissions
-
-                // a file that already exists is required for some operations, and excluded for others
-                // required for read, update, destroy
-                if ( !fileExists && operation === 'read' || 'update' || 'destroy' ) {
-                    throw new Error( 'object does not exist' );
-                }
-
-                // can't exist for write
-                if ( fileExists && operation === 'write' ) {
-                    throw new Error( 'object already exists at that path' );
-                }
-
-                // test permissions against various actions
+                // time to run the permissions
                 if ( operation === 'read' &&
                     !permissions.read ) {
-                    throw new Error( 'user does not have read permissions on this object' );
+                    Promise.reject( 'USER_DOES_NOT_HAVE_READ_PERMISSIONS_ON_THIS_OBJECT' );
                 }
-                if ( operation === 'write' || 'update' &&
+                else if ( operation === 'write' || 'update' &&
                     !permissions.write ) {
-                    throw new Error( 'user does not have write permissions on this object' );
+                    Promise.reject( 'USER_DOES_NOT_HAVE_WRITE_PERMISSIONS_ON_THIS_OBJECT' );
                 }
-                if ( operation === 'destroy' &&
+                else if ( operation === 'destroy' &&
                     !permissions.destroy ) {
-                    throw new Error( 'user does not have write permissions on this object' );
+                    Promise.reject( 'USER_DOES_NOT_HAVE_DESTROY_PERMISSIONS_ON_THIS_OBJECT' );
+                }
+                else {
+                    // if we've made it this far, we're good to go!
+                    Promise.resolve();
                 }
             }
         });
-    }
-    //
-    // AND HERE WE GO
-    let currentPath = '';
-    let meta;
-    let lastParentPath;
-    let pathArray;
-    const promises = [];
-    pathArray = fullPath.split( '/' );
-    pathArray.forEach(( pathComponent ) => {
-        currentPath += '/' + pathComponent;
-        promises.push( verifyPath( currentPath, user ));
-    });
+}
 
+module.exports.verify = ( user, operation, fullPath ) => {
+    // determine if this file (full path) currently exists
+    File.findOne({ $and: [{ name: fullPath }, { userId: user }] }).exec()
+    .then(( file ) => {
+        // a file must exist for certian operations
+        if ( !file && operation === 'read' || 'update' || 'destroy' ) {
+            Promise.reject( 'object does not exist' );
+        }
 
-    Promise.all( promises )
-        .catch(( err ) => {
-            // if this is not the last parent, then an error should produce an error
-            // put here for brevity
-            if ( err.reason !== 'LAST PARENT' ) {
-                Promise.reject( err );
-            }
-            else {
-                // we need the meta and last path
-                meta = err.meta;
-                lastParentPath = err.lastParentPath;
-            }
-        // this is the last parent. continue our logic
-        })
-        .then(() => {
-            verifyLastParent( meta, lastParentPath, fullPath, user );
-        })
+        // can't exist for write
+        else if ( file && operation === 'write' ) {
+            Promise.reject( 'object already exists at that path' );
+            // perform verification on the parnet
+        }
 
-        .then(() => {
-            // if it gets this far it's succeeded!
-            // return the parent path and remaining path
-            Promise.resolve({
-                lastParentPath,
-                remainingPath: pathArray.slice( lastParentPath.split( '/' ), pathArray.length ),
+        // if this is a file, perform verification on the file
+        if ( file ) {
+            verifyPermissions( user, operation, file, false );
+        }
+        // otherwise perform verification on the parent
+        else {
+            return File.findOne({ $and: [{ name: fullPath.split( '/' ).pop() }, { userId: user }] }).exec()
+            .then(( file ) => {
+                // if the parent does not exist, we have a problem
+                if ( !file ) {
+                    Promise.reject( 'INVALID_RESOURCE_PATH' );
+                }
+                else {
+                    verifyPermissions( user, operation, file, true );
+                }
             });
-        })
-        .catch(( err ) => {
-            if ( err.reason !== 'LAST PARENT' ) {
-                Promise.reject( err );
-            }
-        });
+        }
+    });
 };
