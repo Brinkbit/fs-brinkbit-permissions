9e40c2aeaf4574827ed67dbcc22c0582917ed775
JoshF <josh.f@ttbventures.com>
2016-02-23 11:40:53 -0500

    remove all reference to the s3 module from tests. replace path with guid in tests

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@
 const mongoose = require( 'mongoose' );
 const conn = mongoose.connection;
 const Permissions = require( './schemas/permissionSchema.js' );
-const s3Mongo = require( 'fs-s3-mongo' );
-const File = s3Mongo.schema.file;
 mongoose.Promise = Promise;
 
 function mongoConnect() {
@@ -39,98 +37,38 @@ function mongoConnect() {
     }
 }
 
-function verifyPermissions( user, operation, file, isParent ) {
-        // if this is a parent, we need to make sure it's a folder. don't want to insert one file into another
-        // this should have already been caught, but doesn't hurt to double-chcek
-    if ( isParent && file.mimeType !== 'folder' ) {
-        return Promise.reject( 'NOT_ALLOWED' );
-    }
-        // if it passes all the above, return the permissions
-    else {
-        // get the permissions
-        return Permissions.findOne({ $and: [{ resourceId: file._id }, { userId: mongoose.Types.ObjectId( user ) }] }).exec()
-    .then(( permissions ) => {
-        if ( !permissions ) {
-            return Promise.reject( 'NOT_ALLOWED' );
-        }
-        else {
-            // time to run the permissions
-            if ( operation === 'read' &&
-                !permissions.read ) {
-                return Promise.reject( 'NOT_ALLOWED' );
-            }
-            else if ( operation === 'write' &&
-                !permissions.write ||
-                operation === 'update' &&
-                !permissions.write ) {
-                return Promise.reject( 'NOT_ALLOWED' );
-            }
-            else if ( operation === 'destroy' &&
-                !permissions.destroy ) {
-                return Promise.reject( 'NOT_ALLOWED' );
-            }
-            else {
-                // if we've made it this far, we're good to go!
-                return Promise.resolve();
-            }
-        }
-    })
-    .catch(( e ) => {
-        return Promise.reject( e );
-    });
-    }
-}
 
 module.exports.connect = mongoConnect();
 
-module.exports.verify = ( user, operation, fullPath ) => {
-    return new Promise(( resolve, reject ) => {
-        // connect to mongo
-        mongoConnect()
-            .then(() => {
-                // determine if this file (full path) currently exists
-                return File.findOne({ $and: [{ name: fullPath }, { userId: mongoose.Types.ObjectId( user ) }] }).exec();
-            })
-            .then(( file ) => {
-                // a file must exist for certain operations
-                if ( !file && ( operation === 'read' ||
-                    operation === 'update' ||
-                    operation === 'destroy' )) {
-                    return Promise.reject( 'RESOURCE_NOT_FOUND' );
+module.exports.verify = ( userId, operation, guid ) => {
+    Permissions.findOne({ $and: [{ userId }, { guid }] }).exec()
+            .then(( permissions ) => {
+                if ( !permissions ) {
+                    return Promise.reject( 'NOT_ALLOWED' );
                 }
-
-                // can't exist for write
-                else if ( file && operation === 'write' ) {
-                    return Promise.reject( 'RESOURCE_EXISTS' );
-                }
-
-                // if this is a file, perform verification on the file
-                if ( file ) {
-                    return verifyPermissions( user, operation, file, false );
-                }
-                // otherwise perform verification on the parent
                 else {
-                    let fullPathSplit = fullPath.split( '/' );
-                    fullPathSplit.pop();
-                    return File.findOne({ $and: [{ name: fullPathSplit.join( '/' ) + '/' }, { userId: user }] }).exec()
-                        .then(( file ) => {
-                            // if the parent does not exist, we have a problem
-                            // this will also fire if we try to insert one file into another, because the
-                            // trailing '/' will prevent the query from finding anything
-                            if ( !file ) {
-                                return Promise.reject( 'INVALID_RESOURCE_PATH' );
-                            }
-                            else {
-                                return verifyPermissions( user, operation, file, true );
-                            }
-                        });
+                    // time to run the permissions
+                    if ( operation === 'read' &&
+                        !permissions.read ) {
+                        return Promise.reject( 'NOT_ALLOWED' );
+                    }
+                    else if ( operation === 'write' &&
+                        !permissions.write ||
+                        operation === 'update' &&
+                        !permissions.write ) {
+                        return Promise.reject( 'NOT_ALLOWED' );
+                    }
+                    else if ( operation === 'destroy' &&
+                        !permissions.destroy ) {
+                        return Promise.reject( 'NOT_ALLOWED' );
+                    }
+                    else {
+                        // if we've made it this far, we're good to go!
+                        return Promise.resolve();
+                    }
                 }
-            })
-            .then(() => {
-                resolve();
             })
             .catch(( e ) => {
-                reject( e );
+                return Promise.reject( e );
             });
-    });
 };
--- test/tests.js
+++ test/tests.js
@@ -14,9 +14,6 @@ const sinonchai = require( 'sinon-chai' );
 const mime = require( 'mime' );
 const mongoose = require( 'mongoose' );
 const Permissions = require( '../src/schemas/permissionSchema.js' );
-const s3Mongo = require( 'fs-s3-mongo' );
-const File = s3Mongo.schema.file;
-const Meta = s3Mongo.schema.meta;
 const fsPermissions = require( '../src/index.js' );
 const verify = fsPermissions.verify;
 
@@ -29,97 +26,63 @@ const path = [ 'level1', 'level2', 'level3', 'test.txt' ];
 // stub the userid
 let acceptUser = new mongoose.Types.ObjectId();
 let rejectUser = new mongoose.Types.ObjectId();
+const testGuid = 'TESTGUID';
 
 // create the meta and permissions
 const insertFixture = function insertFixture( pathVar ) {
     // for each level:
     const promises = pathVar.map(( value, index, array ) => {
-        // create the meta
-        const file = new File({
-            _id: 'TESTDATA',
-            get mimeType() {
-                let mimeVar;
-                if ( index === array.length ) {
-                    mimeVar = mime.lookup( value.split( '.' ).pop());
-                }
-                else {
-                    mimeVar = 'folder';
-                }
-                return mimeVar;
-            },
-            size: 12345678,
-            dateCreated: new Date(), // https://docs.mongodb.org/v3.0/reference/method/Date/
-            lastModified: new Date(), // https://docs.mongodb.org/v3.0/reference/method/Date/
-            get parents() {
-                if ( index !== 0 ) {
-                    return array[index];
-                }
-            },
-            get name() {
-                let name;
-                if ( array.length === index + 1 ) {
-                    name = array.join( '/' );
-                }
-                else {
-                    name = array.slice( 0, index + 1 ).join( '/' ) + '/';
-                }
-                return name;
-            },
-        });
-        return file.save()
-            .then(( fileObj ) => {
                 // create the permission record for the good user
-                const goodPermissions = new Permissions({
-                    get resourceType() {
-                        let resourceType;
-                        if ( fileObj.mimeType === 'folder' ) {
-                            resourceType = 'folder';
-                        }
-                        else {
-                            resourceType = 'file';
-                        }
-                        return resourceType;
-                    },  // project or file/folder and we can easily add additional resource types later
-                    resourceId: fileObj.id, // links to metadata id or project id
-                    appliesTo: 'user', // 'user', 'group', 'public'
-                    userId: acceptUser,
-                    groupId: null, // if applies to group
-                    read: true,
-                    write: true,
-                    destroy: true,
-                    // share: [String], add additional user with default permissions for collaboration
-                    manage: true, // update/remove existing permissions on resource
-                });
-                const badPermissions = new Permissions({
-                    get resourceType() {
-                        let resourceType;
-                        if ( fileObj.mimeType === 'folder' ) {
-                            resourceType = 'folder';
-                        }
-                        else {
-                            resourceType = 'file';
-                        }
-                        return resourceType;
-                    },  // project or file/folder and we can easily add additional resource types later
-                    resourceId: fileObj.id, // links to metadata id or project id
-                    appliesTo: 'user', // 'user', 'group', 'public'
-                    userId: rejectUser,
-                    groupId: null, // if applies to group
-                    read: false,
-                    write: false,
-                    destroy: false,
-                    // share: [String], add additional user with default permissions for collaboration
-                    manage: false, // update/remove existing permissions on resource
-                });
-                // create the good file record
-                return Promise.all([
-                    goodPermissions.save(),
-                    badPermissions.save(),
-                ]);
-            })
-            .catch(( e ) => {
-                return Promise.reject( e );
-            });
+        const goodPermissions = new Permissions({
+            get resourceType() {
+                let resourceType;
+                if ( index !== array.length ) {
+                    resourceType = 'folder';
+                }
+                else {
+                    resourceType = 'file';
+                }
+                return resourceType;
+            },  // project or file/folder and we can easily add additional resource types later
+            resourceId: testGuid, // links to metadata id or project id
+            appliesTo: 'user', // 'user', 'group', 'public'
+            userId: acceptUser,
+            groupId: null, // if applies to group
+            read: true,
+            write: true,
+            destroy: true,
+            // share: [String], add additional user with default permissions for collaboration
+            manage: true, // update/remove existing permissions on resource
+        });
+        const badPermissions = new Permissions({
+            get resourceType() {
+                let resourceType;
+                if ( index !== array.length ) {
+                    resourceType = 'folder';
+                }
+                else {
+                    resourceType = 'file';
+                }
+                return resourceType;
+            },  // project or file/folder and we can easily add additional resource types later
+            resourceId: testGuid, // links to metadata id or project id
+            appliesTo: 'user', // 'user', 'group', 'public'
+            userId: rejectUser,
+            groupId: null, // if applies to group
+            read: false,
+            write: false,
+            destroy: false,
+            // share: [String], add additional user with default permissions for collaboration
+            manage: false, // update/remove existing permissions on resource
+        });
+        // create the good file record
+        return Promise.all([
+            goodPermissions.save(),
+            badPermissions.save(),
+        ]);
+    })
+    .catch(( e ) => {
+        return Promise.reject( e );
     });
     return Promise.all( promises );
 };
@@ -138,19 +101,7 @@ describe( 'verify', ( ) => {
 
 
     afterEach( function afterEach( done ) {
-        // make an array of all test meta ids
-        let ids;
-        Meta.find({ guid: 'TESTDATA' }).exec()
-        .then(( docs ) => {
-            ids = docs.map( function mapId( item ) {
-                return item._id;
-            });
-            const p1 = Meta.remove({ _id: { $in: ids } }).exec();
-            const p2 = Permissions.remove({ resourceId: { $in: ids } }).exec();
-            const p3 = File.remove({ metaDataId: { $in: ids } }).exec();
-            // now remove all the things
-            return Promise.all([ p1, p2, p3 ]);
-        })
+        Permissions.remove({ resourceId: 'TESTGUID' }).exec()
         .then(() => {
             done();
         })
@@ -163,42 +114,31 @@ describe( 'verify', ( ) => {
     acceptUser = acceptUser.toString();
     rejectUser = rejectUser.toString();
     it( 'should allow reading a file with correct permissions', () => {
-        // return expect(Promise.resolve({ foo: "bar" })).to.be.fulfilled;
-        return expect( verify( acceptUser, 'read', 'level1/level2/level3/test.txt' )).to.be.fulfilled;
+        return expect( verify( acceptUser, 'read', testGuid )).to.be.fulfilled;
     });
     it( 'should reject reading a file with incorrect permissions', () => {
-        return expect( verify( rejectUser, 'read', 'level1/level2/level3/test.txt' ))
+        return expect( verify( rejectUser, 'read', testGuid ))
             .to.be.rejectedWith( 'NOT_ALLOWED' );
     });
     it( 'should allow updating a file with correct permissions', () => {
-        return expect( verify( acceptUser, 'update', 'level1/level2/level3/test.txt' )).to.be.fulfilled;
+        return expect( verify( acceptUser, 'update', testGuid )).to.be.fulfilled;
     });
     it( 'should reject updating a file with incorrect permissions', () => {
-        return expect( verify( rejectUser, 'update', 'level1/level2/level3/test.txt' ))
+        return expect( verify( rejectUser, 'update', testGuid ))
             .to.be.rejectedWith( 'NOT_ALLOWED' );
     });
     it( 'should allow destroying a file with correct permissions', () => {
-        return expect( verify( acceptUser, 'destroy', 'level1/level2/level3/test.txt' )).to.be.fulfilled;
+        return expect( verify( acceptUser, 'destroy', testGuid )).to.be.fulfilled;
     });
     it( 'should reject destroying a file with incorrect permissions', () => {
-        return expect( verify( rejectUser, 'destroy', 'level1/level2/level3/test.txt' ))
+        return expect( verify( rejectUser, 'destroy', testGuid ))
             .to.be.rejectedWith( 'NOT_ALLOWED' );
     });
     it( 'should allow insertion of a file with correct permissions on the parent folder', () => {
-        return expect( verify( acceptUser, 'write', 'level1/level2/permissions1.txt' )).to.be.fulfilled;
+        return expect( verify( acceptUser, 'write', testGuid )).to.be.fulfilled;
     });
     it( 'should reject insertion of a file with incorrect permissions on the parent folder', () => {
-        return expect( verify( rejectUser, 'write', 'level1/level2/permissions2.txt' ))
+        return expect( verify( rejectUser, 'write', testGuid ))
             .to.be.rejectedWith( 'NOT_ALLOWED' );
-    });
-    // should not treat a file as a folder
-    it( 'not allow insertion of a file into another file', () => {
-        return expect( verify( acceptUser, 'write', 'level1/level2/level3/test.txt/nestedTest.txt' ))
-            .to.be.rejectedWith( 'INVALID_RESOURCE_PATH' );
-    });
-    // should not create a duplicate file
-    it( 'not allow insertion of a duplicate file', () => {
-        return expect( verify( acceptUser, 'write', 'level1/level2/level3/test.txt' ))
-            .to.be.rejectedWith( 'RESOURCE_EXISTS' );
     });
 });
