1d11444c1d24929ef98d4cac354c6d7c27bdeacd
Brian Silvia <brian@brinkbit.com>
2016-05-11 20:55:26 -0400

    Add logging and standardize mongodb

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@
 
 const mongoose = require( 'mongoose' );
 const conn = mongoose.connection;
+const logger = require( 'brinkbit-logger' )({ __filename, transport: 'production' });
+const mongoConfig = require( 'the-brink-mongodb' );
 const Permissions = require( './schemas/permissionSchema.js' );
 mongoose.Promise = Promise;
 
 const connect = () => {
     return new Promise(( resolve, reject ) => {
-        // Already connected?
-        if ( conn.readyState === 1 ) return resolve();
+        if ( conn.readyState === 1 ) {
+            logger.warning( 'Already connected. Resolving.' );
+            return resolve();
+        }
 
-        // if not, connect
-        const IP = process.env.NODE_ENV === 'development' ? process.env.ip : 'localhost';
-        mongoose.connect( `mongodb://${IP}:27017` );
+        const mongodbURI = mongoConfig.mongodb.uri;
+        logger.info( `Attempting to connect to: ${mongodbURI}` );
+        mongoose.connect( mongodbURI );
         conn.on( 'error', ( err ) => reject( err ));
         conn.on( 'connected', resolve );
     });
@@ -20,12 +24,16 @@ const connect = () => {
 
 
 const verify = ( guid, userId, operation ) => {
+    logger.info( `Checking ${operation} permission on ${guid} for ${userId}` );
     return Permissions.findOne({ $and: [{ userId }, { resourceId: guid }] }).exec()
     .then( permissions => {
         // Check for returned permissions, and that it matches the operation
         if ( !permissions || ( operation === 'read' && !permissions.read ) || (( operation === 'write' || operation === 'update' ) && !permissions.write ) || ( operation === 'destroy' && !permissions.destroy )) {
+            logger.info( 'User does not have permission, rejecting.' );
             return Promise.reject( 'NOT_ALLOWED' );
         }
+
+        logger.info( 'User has permission, resolving.' );
     })
     .catch( err => Promise.reject( err ));
 };
